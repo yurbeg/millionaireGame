a6b078bc9814cd0d5d224f3fa997fd6c1517c53a
yurbeg <yur.beglaryan@gmail.com>
2024-11-09 00:19:38 +0400

    fixed

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
-  import Main from './components/main';
-  import Login from './pages/auth/login';
-  import Register from './pages/auth/register';
-  import { Routes,Route,Navigate} from "react-router-dom"
-  import { useState,useEffect } from 'react';
-  import { auth } from './services/firbase'
-  import {onAuthStateChanged} from "firebase/auth"
+import Main from './components/main';
+import Login from './pages/auth/login';
+import Register from './pages/auth/register';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { auth } from './services/firbase';
+import { onAuthStateChanged } from 'firebase/auth';
 
-  import './App.css';
+import './App.css';
 
-  function App() {
-    const [isAuth,setIsAuth] = useState(false)
-    const [loading, setLoading] = useState(true);
+function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-      const unsubscribe = onAuthStateChanged(auth, (user) => {
-        if (user) {
-          setIsAuth(true);
-        } else {
-          setIsAuth(false);
-        }
-        setLoading(false); 
-      });
-  
-      return () => unsubscribe();
-    }, []);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuth(true);
+      } else {
+        setIsAuth(false);
+      }
+      setLoading(false);
+    });
 
-    return (
-      <div className="App">
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <div className="App">
       {loading ? (
-        <div>Loading...</div> 
+        <div>Loading...</div>
       ) : (
-        <Routes>
-          <Route path='/' element={isAuth ? <Main /> : <Navigate to="/login" />} />
-          <Route path='/login' element={isAuth ? <Navigate to="/" /> : <Login />} />
-          <Route path='/register' element={isAuth ? <Navigate to="/login" /> : <Register />} />
-        </Routes>
+        <>
+          <Routes>
+            <Route path="/" element={isAuth ? <Main /> : <Navigate to="/login" />} />
+            <Route path="/login" element={isAuth ? <Navigate to="/" /> : <Login />} />
+            <Route path="/register" element={isAuth ? <Navigate to="/login" /> : <Register />} />
+          </Routes>
+        </>
       )}
     </div>
-    );
-  }
+  );
+}
 
-  export default App;
+export default App;
