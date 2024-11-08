0e5490af3aaacd9fa96fb419b70f8015d0944f10
yurbeg <yur.beglaryan@gmail.com>
2024-11-08 23:03:00 +0400

    add responsive styles

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,124 +1,144 @@
 import { useEffect, useState } from "react";
-import { questions,backgroundGragient,backgroundGragientHover} from "../../core/constants/constants";
+import {
+  questions,
+  backgroundGragient,
+  backgroundGragientHover,
+} from "../../core/constants/constants";
 import Option from "../option";
 import { Button } from "antd";
+import { PhoneOutlined } from '@ant-design/icons'
 import Points from "../points";
 import { fibonacciArr } from "../../core/constants/constants";
+
 import "./index.css";
 
 const Main = () => {
   const [index, setIndex] = useState(0);
-  const [lose,setLose] = useState(false)
-  const [win,setWin] = useState(false)
+  const [lose, setLose] = useState(false);
+  const [win, setWin] = useState(false);
   const [isDisabled, setIsDisabled] = useState(false);
   const [fiftyFiftyUsed, setFiftyFiftyUsed] = useState(false); // Состояние для отслеживания использования 50:50
-  const [callToFriend,setCallToFriend] = useState(false)
+  const [callToFriend, setCallToFriend] = useState(false);
   const handleClick = (e, i) => {
     e.target.style.background = "orange";
     setIsDisabled(true);
     setTimeout(() => {
       if (questions[index].answer === i) {
         e.target.style.background = "green";
-        setTimeout(() => {          
-          if(index+1 === questions.length ){
-            setWin(true)
-          }else{
-          document.getElementById(`${index}`).style.background = "orange"
-
+        setTimeout(() => {
+          if (index + 1 === questions.length) {
+            setWin(true);
+          } else {
+            document.getElementById(`${index}`).style.background = "orange";
           }
-          e.target.style.background = backgroundGragient
-          setIndex((prev) => prev + 1);      
-          if(index >= 1){
-            document.getElementById(`${index-1}`).style.background = ""
+          e.target.style.background = backgroundGragient;
+          setIndex((prev) => prev + 1);
+          if (index >= 1) {
+            document.getElementById(`${index - 1}`).style.background = "";
           }
         }, 1000);
       } else {
         e.target.style.background = "red";
         setTimeout(() => {
-          setLose(true)
+          setLose(true);
           e.target.style.background = backgroundGragient;
         }, 1000);
       }
-      e.target.addEventListener('mouseover', (e) => {
-        e.target.style.background = backgroundGragientHover
-      })
-      e.target.addEventListener('mouseleave', (e) => {
-        e.target.style.background = backgroundGragient; 
+      e.target.addEventListener("mouseover", (e) => {
+        e.target.style.background = backgroundGragientHover;
+      });
+      e.target.addEventListener("mouseleave", (e) => {
+        e.target.style.background = backgroundGragient;
       });
       setIsDisabled(false);
     }, 1500);
   };
 
   const handleFiftyFifty = () => {
-    if (fiftyFiftyUsed || !questions[index]) return; 
-    setFiftyFiftyUsed(true); 
+    if (fiftyFiftyUsed || !questions[index]) return;
+    setFiftyFiftyUsed(true);
 
-    const correctAnswer = questions[index].options[questions[index].answer]
-    const randomIndexes = []
-    let count = 2
-    for(let i = 0;i < count;i++){
-      const randomIndex = Math.floor(Math.random() * questions[index].options.length);
-      
+    const correctAnswer = questions[index].options[questions[index].answer];
+    const randomIndexes = [];
+    let count = 2;
+    for (let i = 0; i < count; i++) {
+      const randomIndex = Math.floor(
+        Math.random() * questions[index].options.length
+      );
+
       console.log(randomIndex, questions[index].answer);
-      if(randomIndex !== questions[index].answer && !randomIndexes.includes(randomIndex)){
-        questions[index].options.splice(randomIndex,1)
-        
-        randomIndexes.push(randomIndex)
-      }
-      else {
-        count ++
+      if (
+        randomIndex !== questions[index].answer &&
+        !randomIndexes.includes(randomIndex)
+      ) {
+        questions[index].options.splice(randomIndex, 1);
+
+        randomIndexes.push(randomIndex);
+      } else {
+        count++;
       }
     }
-    questions[index].answer =  questions[index].options.indexOf(correctAnswer)
+    questions[index].answer = questions[index].options.indexOf(correctAnswer);
     console.log(correctAnswer);
-    
-
   };
 
   const handleCallToFriend = () => {
-    alert(`Ճիշտ պատասխանն է ${questions[index].options[questions[index].answer]}` )    
-    setCallToFriend(true)
-  }
+    alert(
+      `Ճիշտ պատասխանն է ${questions[index].options[questions[index].answer]}`
+    );
+    setCallToFriend(true);
+  };
   useEffect(() => {}, []);
   return (
-    <div style={{
-      display:"flex",
-      width:"100%",
-      justifyContent:"center"
-    }}>
-      
-    <div className="main_container">
-      <div className="question_container">{questions[index]?.question}</div>
-      <div className="options_container">
-        {(!lose && !win) && questions[index]?.options.map((answer, i) => (
-          <Option
-            key={i}
-            answer={answer}
-            i={i}
-            handleClick={handleClick}
-            isDisabled={isDisabled}
-          />
-        ))}
-        {lose && <div  className="result_container">Ձեր շահած գումարի չափը կազմում է {fibonacciArr[index-1]?fibonacciArr[index-1]*1000:0} դրամ</div>}
-        {win && <div  className="result_container">Շնորհավորում ենք, դուք հաղթել եք {fibonacciArr[fibonacciArr.length-1]*1000} դրամ</div>}
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        justifyContent: "center",
+      }}
+    >
+      <div className="main_container">
+        <div className="question_container">{questions[index]?.question}</div>
+        <div className="options_container">
+          {!lose &&
+            !win &&
+            questions[index]?.options.map((answer, i) => (
+              <Option
+                key={i}
+                answer={answer}
+                i={i}
+                handleClick={handleClick}
+                isDisabled={isDisabled}
+              />
+            ))}
+          {lose && (
+            <div className="result_container">
+              Ձեր շահած գումարի չափը կազմում է{" "}
+              {fibonacciArr[index - 1] ? fibonacciArr[index - 1] * 1000 : 0}{" "}
+              դրամ
+            </div>
+          )}
+          {win && (
+            <div className="result_container">
+              Շնորհավորում ենք, դուք հաղթել եք{" "}
+              {fibonacciArr[fibonacciArr.length - 1] * 1000} դրամ
+            </div>
+          )}
+        </div>
+        <div className="help_elements">
+          {!fiftyFiftyUsed && (
+            <Button className="fifty_fifty_button" onClick={handleFiftyFifty}>
+              50:50
+            </Button>
+          )}
+          {!callToFriend && (
+            <Button className="fifty_fifty_button" onClick={handleCallToFriend}>
+              <PhoneOutlined />
+            </Button>
+          )}
+        </div>
       </div>
-      <div className="help_elements">
-      {!fiftyFiftyUsed && (
-        <Button className="fifty_fifty_button" onClick={handleFiftyFifty}>
-          50:50
-        </Button>
-      )}
-        {!callToFriend && (
-        <Button className="fifty_fifty_button" onClick={handleCallToFriend}>
-          Զանգ ընկերոջը
-        </Button>
-      )}
-
-      </div>
-    
-    </div>
-      <Points/>
-
+      <Points />
     </div>
   );
 };
